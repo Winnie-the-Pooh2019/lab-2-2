9199ac4ebf6296220ef34b0e7a7d2c6013ec2719
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2025-02-21 17:14:37 +0700

    init commit

--- main.cpp
+++ main.cpp
@@ -1,25 +1,10 @@
 #include <iostream>
 
-#include "bell.h"
-#include "separator.h"
-
 using namespace std;
 
 int main()
 {
-    OddEvenSeparator separator;
 
-    separator.add_number(12);
-    separator.add_number(13);
-    separator.add_number(14);
-    separator.add_number(15);
-    separator.add_number(16);
-    separator.add_number(17);
-    separator.add_number(18);
-    separator.add_number(19);
-
-    separator.even();
-    separator.odd();
 
     return 0;
 }
